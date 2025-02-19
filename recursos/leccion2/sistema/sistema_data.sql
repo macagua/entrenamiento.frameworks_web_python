8e17e43907f7d404c431e9e1ae0b8c07c5d25962
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-02-19 19:41:29 +0100

    Upgraded the sistema section

--- recursos/leccion2/sistema/sistema_data.sql
+++ recursos/leccion2/sistema/sistema_data.sql
@@ -1,18 +1,22 @@
 -- Insertar registros de tabla clientes
-INSERT INTO clientes(id, nombre, apellido, codigo_postal, telefono)
-VALUES (1,'Leonardo','Caballero','5001', '+58-412-4734567');
-INSERT INTO clientes(id, nombre, apellido, codigo_postal, telefono)
-VALUES (2,'Ana','Poleo','6302', '+58-426-5831297');
-INSERT INTO clientes(id, nombre, apellido, codigo_postal, telefono)
-VALUES (3,'Pedro','Lopez','4001', '+58-414-2360943');
+INSERT INTO clientes (id, nombre, apellido, codigo_postal, telefono)
+VALUES (1, 'Leonardo', 'Caballero', '5001', '+58-412-4734567');
+INSERT INTO clientes (id, nombre, apellido, codigo_postal, telefono)
+VALUES (2, 'Ana', 'Poleo', '6302', '+58-426-5831297');
+INSERT INTO clientes (id, nombre, apellido, codigo_postal, telefono)
+VALUES (3, 'Manuel', 'Matos', '4001', '+58-414-2360943');
 
 -- Seleccionar registros
 SELECT * FROM clientes;
-SELECT * FROM clientes WHERE id = 1;
+SELECT * FROM clientes
+WHERE id = 1;
 
 -- Actualizar registros
-UPDATE clientes SET codigo_postal = 5051 WHERE id = 1;
-UPDATE clientes SET codigo_postal = 6303 WHERE id = 2;
+UPDATE clientes SET codigo_postal = 5051
+WHERE id = 1;
+UPDATE clientes SET codigo_postal = 6303
+WHERE id = 2;
 
 -- Eliminar registros
-DELETE FROM clientes WHERE id = 2;
+DELETE FROM clientes
+WHERE id = 2;
