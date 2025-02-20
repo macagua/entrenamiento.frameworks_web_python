aa5fd82adaa7e61403239959be54fe6257b6af19
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-02-20 17:10:30 +0100

    Updated the leccion2 section

--- recursos/leccion2/sqlacodegen/sistema/main.py
+++ recursos/leccion2/sqlacodegen/sistema/main.py
@@ -1,31 +1,67 @@
 """Módulo principal del programa"""
 
 from db import session
-from models import Estados, Ciudades
+from models import Estados, Ciudades, Clientes, Productos, Pedidos
 
 
 def consulta_estados():
     """Consulta todos los estados"""
 
-    print("¡Consulta todos los estados!")
+    print("\n📜 Lista de Estados:\n")
     # SELECT * FROM estados;
-    estados = session.query(Estados).all()
-    for estado in estados:
-        print(estado)
-    print("")
+    todos_estados = session.query(Estados).all()
+    for cada_estado in todos_estados:
+        print(f"{cada_estado}")
 
 
 def consulta_ciudades():
     """Consulta todas las ciudades"""
 
-    print("¡Consulta todas las ciudades!")
+    print("\n📜 Lista de Ciudades:\n")
     # SELECT * FROM ciudades;
-    ciudades = session.query(Ciudades).all()
-    for ciudad in ciudades:
-        print(ciudad)
-    print("")
+    todos_ciudades = session.query(Ciudades).all()
+    for cada_ciudad in todos_ciudades:
+        print(f"{cada_ciudad}")
+
+
+def consulta_clientes():
+    """Consulta todas las clientes"""
+
+    print("\n📜 Lista de Clientes:\n")
+    # SELECT * FROM clientes;
+    todos_clientes = session.query(Clientes).all()
+    for cada_cliente in todos_clientes:
+        print(f"{cada_cliente}")
+
+
+def consulta_pedidos():
+    """Consulta todas las pedidos"""
+
+    print("\n📜 Lista de Pedidos:\n")
+    # SELECT * FROM pedidos;
+    todos_pedidos = session.query(Pedidos).all()
+    for cada_pedido in todos_pedidos:
+        print(f"{cada_pedido}")
+
+
+def consulta_productos():
+    """Consulta todas las productos"""
+
+    print("\n📜 Lista de Productos:\n")
+    # SELECT * FROM productos;
+    todos_productos = session.query(Productos).all()
+    for cada_producto in todos_productos:
+        print(f"{cada_producto}")
 
 
 if __name__ == "__main__":
+    # Consulta de productos
     consulta_estados()
+    # Consulta de ciudades
     consulta_ciudades()
+    # Consulta de clientes
+    consulta_clientes()
+    # Consulta de pedidos
+    consulta_pedidos()
+    # Consulta de pedidos
+    consulta_productos()
--- recursos/leccion2/sqlalchemy/productos/db.py
+++ recursos/leccion2/sqlalchemy/productos/db.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,11 +11,14 @@ load_dotenv()
 DB_PATH = os.path.dirname(os.path.abspath(__file__)) + os.sep
 DB_FILE = os.getenv("DB", "productos.sqlite3")
 
+engine = None
+
 # Configurar conexiones entre SQLAlchemy y SQLite3 DB API
 engine = create_engine(f"sqlite:///{DB_PATH}{DB_FILE}")
 
-# Crear sesión con el engine de base de datos
-Session = sessionmaker(bind=engine)
-session = Session()
-
-Base = declarative_base()
+if "engine" in globals():
+    # Crear sesión con el engine de base de datos
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Crear base declarativa
+    Base = declarative_base()
